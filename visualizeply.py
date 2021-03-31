74e11d57f5e0c505d62aaf477a17bb554515c351
akhawaja2014 <akhawaja2014@gmail.com>
2021-03-31 16:04:29 +0200

    pcl file updated

--- visualizeply.py
+++ visualizeply.py
@@ -3,7 +3,7 @@ import open3d as o3d
    
    
 print("Load a ply point cloud, print it, and render it")
-pcd = o3d.read_point_cloud("1.ply")
+pcd = o3d.read_point_cloud("out.ply")
 print(pcd)
 print(np.asarray(pcd.points))
 o3d.draw_geometries([pcd])
